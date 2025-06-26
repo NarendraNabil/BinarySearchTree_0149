7968ef450fa3f7dc73457095e00544d7f5a35e59
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-06-26 18:15:19 +0700

    step 3 make the left and right child of the new node point to NULL

--- binarytree.cpp
+++ binarytree.cpp
@@ -57,3 +57,7 @@ public:
         //step 2: Assign value to the data field of new node
         newNode->info = x;
 
+        //step 3: Make the left and right child of the new node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
