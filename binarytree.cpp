741823c4864eb86da8c0b5cccb381b1cd7a2b010
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-06-26 18:15:59 +0700

    step 4 locate the node which will be the parent of the node to be inserted

--- binarytree.cpp
+++ binarytree.cpp
@@ -61,3 +61,8 @@ public:
         newNode->leftchild = nullptr;
         newNode->rightchild = nullptr;
 
+        //step 4: Locate the node which will be the parent of the node to be inserted
+        Node *parent = nullptr;
+        Node *currentNode = nullptr;
+        search(x, parent, currentNode);
+
